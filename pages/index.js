90589012337129ace5f9b3f8ee249aa4875785a8
@martin.a.stoyanov <martin.a.stoyanov@gmail.com>
2019-10-25 20:11:11 -0400

    basic line chart moved to its own component

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,14 @@
 import 'react-d3-components';
-import LineChartContainer from "../components/LineChartContainer";
+import LineChartContainer from '../components/LineChartContainer';
 
 // import 'ETHSX.csv' from '../datasets';
 
-export default class extends React.Component { 
+export default class extends React.Component {
   render() {
-  return (
-    <div>
-      <LineChartContainer/>
-    </div>
-  );
-}
+    return (
+      <div>
+        <LineChartContainer />
+      </div>
+    );
+  }
 }
